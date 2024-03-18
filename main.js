aa00b912a10655fe7fc2f99cce877eefe341dd46
Aliceit210 <128842074+Aliceit210@users.noreply.github.com>
2024-03-18 07:26:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    const button = document.getElementById('menu-button');
+    const menu = document.getElementById('menu');
+
+    button.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    });
+};
+
+ 
